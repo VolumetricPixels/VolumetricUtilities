569ddd8cc050931f625734bb17d256555e587ff8
Ollie <dzineit@live.co.uk>
2012-09-30 13:17:38 +0100

    No point in an ArrayIterator interface with implementation.
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/com/volumetricpixels/utils/array/ArrayIterator.java
+++ src/com/volumetricpixels/utils/array/ArrayIterator.java
@@ -2,10 +2,56 @@ package com.volumetricpixels.utils.array;
 
 import java.util.Iterator;
 
-public interface ArrayIterator<E> extends Iterator<E> {
-    public E prev();
+import com.volumetricpixels.utils.Utilities;
 
-    public int nextIndex();
+public class ArrayIterator<E> implements Iterator<E> {
+    private Object[] am;
+    private int index = 0;
 
-    public int prevIndex();
+    private int[] removed;
+    private int removedIndex = 0;
+
+    public ArrayIterator(Object[] array) {
+        this.am = array;
+        this.removed = new int[array.length];
+    }
+
+    public boolean hasNext() {
+        try {
+            return am[index + 1] != null;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return false;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public E next() {
+        do {
+            index++;
+        } while (Utilities.intArrayContains(removed, index));
+
+        return (E) am[index];
+    }
+
+    public void remove() {
+        removed[removedIndex] = index;
+        removedIndex++;
+    }
+
+    @SuppressWarnings("unchecked")
+    public E prev() {
+        do {
+            index--;
+        } while (Utilities.intArrayContains(removed, index));
+
+        return (E) am[index];
+    }
+
+    public int nextIndex() {
+        return index + 1;
+    }
+
+    public int prevIndex() {
+        return index - 1;
+    }
 }
--- src/com/volumetricpixels/utils/array/ArrayManipulator.java
+++ src/com/volumetricpixels/utils/array/ArrayManipulator.java
@@ -1,7 +1,5 @@
 package com.volumetricpixels.utils.array;
 
-import com.volumetricpixels.utils.Utilities;
-
 /**
  * Simple utility for easily controlling an Array when after-compilation array
  * type doesn't matter too much.
@@ -50,62 +48,6 @@ public class ArrayManipulator<E> {
     }
 
     public ArrayIterator<E> iterator() {
-        return new ArrayIteratorImpl<E>(this.array);
-    }
-
-    public static class ArrayIteratorImpl<E> implements ArrayIterator<E> {
-        private Object[] am;
-        private int index = 0;
-
-        private int[] removed;
-        private int removedIndex = 0;
-
-        public ArrayIteratorImpl(Object[] array) {
-            this.am = array;
-            this.removed = new int[array.length];
-        }
-
-        @Override
-        public boolean hasNext() {
-            try {
-                return am[index + 1] != null;
-            } catch (ArrayIndexOutOfBoundsException e) {
-                return false;
-            }
-        }
-
-        @Override
-        public E next() {
-            do {
-                index++;
-            } while (Utilities.intArrayContains(removed, index));
-
-            return (E) am[index];
-        }
-
-        @Override
-        public void remove() {
-            removed[removedIndex] = index;
-            removedIndex++;
-        }
-
-        @Override
-        public E prev() {
-            do {
-                index--;
-            } while (Utilities.intArrayContains(removed, index));
-
-            return (E) am[index];
-        }
-
-        @Override
-        public int nextIndex() {
-            return index + 1;
-        }
-
-        @Override
-        public int prevIndex() {
-            return index - 1;
-        }
+        return new ArrayIterator<E>(this.array);
     }
 }
